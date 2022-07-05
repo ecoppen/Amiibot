bc5ff8b2084f620395ff752ec7b0aaf3bc385ef7
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-07-05 13:06:27 +0100

    Update amiibot.py
    `StockistManager` for loading every stockist and notifying user that they're loaded

--- amiibot.py
+++ amiibot.py
@@ -1,11 +1,11 @@
 import logging
 import os
-import time
 from pathlib import Path
 
 from config.config import load_config
 from database import Database
 from messenger.manager import MessageManager
+from stockist.manager import StockistManager
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -16,22 +16,10 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 config_path = Path("config", "config.json")
-config = load_config(config_path)
+config = load_config(path=config_path)
 log.info(f"{config_path} loaded")
 
-database = Database(config.database)
+database = Database(config=config.database)
 
-messengers = MessageManager(config.messengers)
-
-from stockist.bestbuy import Bestbuy
-from stockist.bestbuyca import BestbuyCA
-from stockist.game import Game
-from stockist.nintendouk import NintendoUK
-from stockist.playasia import PlayAsia
-from stockist.shopto import Shopto
-
-x = PlayAsia()
-for each in messengers.all_messengers:
-    for item in x.get_amiibo():
-        response = each.send_embed_message(embed_data=item)
-        time.sleep(0.5)
+messengers = MessageManager(config=config.messengers)
+stockists = StockistManager(config=config.stockists, messengers=messengers)
