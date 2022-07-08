047ca322287e863f3fe3db3730415a3fa844b912
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-07-08 10:20:38 +0100

    Update manager.py
    pass messengers to each stockist that it relates to

--- stockist/manager.py
+++ stockist/manager.py
@@ -14,50 +14,59 @@ log = logging.getLogger(__name__)
 
 
 class StockistManager:
-    def __init__(self, config, messengers) -> None:
+    def __init__(self, messengers) -> None:
         self.all_stockists = []
         self.messengers = messengers
+        self.relationships: dict[str, list[str]] = {}
 
-        for stockist in config:
+        for messenger in messengers.all_messengers:
+            for stockist in messenger.stockists:
+                if stockist not in self.relationships:
+                    self.relationships[stockist] = []
+                if messenger.name not in self.relationships[stockist]:
+                    self.relationships[stockist].append(messenger.name)
+
+        for stockist in self.relationships:
+            messengers = self.relationships[stockist]
             if stockist == "bestbuy.com":
-                bestbuy = Bestbuy()
+                bestbuy = Bestbuy(messengers=messengers)
                 self.all_stockists.append(bestbuy)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "bestbuy.ca":
-                bestbuyca = BestbuyCA()
+                bestbuyca = BestbuyCA(messengers=messengers)
                 self.all_stockists.append(bestbuyca)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "gamestop.com":
-                gamestop = Gamestop()
+                gamestop = Gamestop(messengers=messengers)
                 self.all_stockists.append(gamestop)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "game.co.uk":
-                game = Game()
+                game = Game(messengers=messengers)
                 self.all_stockists.append(game)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "mecca-japan.com":
-                meccajapan = MecchaJapan()
+                meccajapan = MecchaJapan(messengers=messengers)
                 self.all_stockists.append(meccajapan)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "nintendo.co.uk":
-                nintendouk = NintendoUK()
+                nintendouk = NintendoUK(messengers=messengers)
                 self.all_stockists.append(nintendouk)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "play-asia.com":
-                playasia = PlayAsia()
+                playasia = PlayAsia(messengers=messengers)
                 self.all_stockists.append(playasia)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "shopto.net":
-                shopto = Shopto()
+                shopto = Shopto(messengers=messengers)
                 self.all_stockists.append(shopto)
                 log.info(f"Now tracking {stockist}")
             elif stockist == "thesource.ca":
-                thesource = TheSource()
+                thesource = TheSource(messengers=messengers)
                 self.all_stockists.append(thesource)
                 log.info(f"Now tracking {stockist}")
 
-        stokist_check = self.check_for_one_stockist()
-        if stokist_check:
+        stockist_check = self.check_for_one_stockist()
+        if stockist_check:
             stockists = ", ".join([stockist.name for stockist in self.all_stockists])
             self.messengers.send_message_to_all_messengers(
                 f"Now tracking {len(self.all_stockists)} sites: {stockists}"
@@ -65,6 +74,6 @@ class StockistManager:
 
     def check_for_one_stockist(self):
         if len(self.all_stockists) < 1:
-            log.error("No messengers were set to true")
+            log.error("No stockists were set to be scraped")
             return False
         return True
