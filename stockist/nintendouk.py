2547e0bc6a9e9da32a53197608168e2a74a6c563
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-09-09 16:58:47 +0100

    Update nintendouk.py
    Get all from the api, only stop when empty list is returned

--- stockist/nintendouk.py
+++ stockist/nintendouk.py
@@ -25,135 +25,128 @@ class NintendoUK(Stockist):
     name = "Nintendo UK"
 
     def get_amiibo(self):
-        options = ["newest-products", "price-high-to-low", "price-low-to-high"]
-        offsets = [0, 24, 48]
-
         all_found = []
+        complete = False
 
-        for option in options:
-            for offset in offsets:
-                self.params["sort"] = option
-                self.params["offset"] = offset
+        while not complete:
+            response = self.scrape(url=self.base_url, payload=self.params)
 
-                response = self.scrape(url=self.base_url, payload=self.params)
+            try:
+                cards = json.loads(response.content.decode("utf-8"))
+            except json.JSONDecodeError as exc:
+                log.error(
+                    f"Invalid JSON: {exc.msg}, line {exc.lineno}, column {exc.colno}"
+                )
+                cards = []
+            except AttributeError as e:
+                log.error(f"Invalid attribute: {e}")
+                cards = []
 
-                try:
-                    cards = json.loads(response.content.decode("utf-8"))
-                except json.JSONDecodeError as exc:
-                    log.error(
-                        f"Invalid JSON: {exc.msg}, line {exc.lineno}, column {exc.colno}"
-                    )
-                    cards = []
-                except AttributeError as e:
-                    log.error(f"Invalid attribute: {e}")
-                    cards = []
+            if len(cards) > 0:
+                if len(cards["data"]["products"]) == 0:
+                    complete = True
+                for card in cards["data"]["products"]:
+                    name = card["name"]
+                    price = card["pricePerUnit"]
+                    img = card["c_productImages"][0]
+                    url = card["path"]
+                    stock = card["c_availabilityModel"]["type"]
 
-                if len(cards) > 0:
-                    for card in cards["data"]["products"]:
-                        name = card["name"]
-                        price = card["pricePerUnit"]
-                        img = card["c_productImages"][0]
-                        url = card["path"]
-                        stock = card["c_availabilityModel"]["type"]
+                    found = {
+                        "Colour": 0x0000FF,
+                        "Title": name,
+                        "Image": f"https://assets.nintendo.eu/image/upload/v1654696477/{img}",
+                        "URL": f"https://store.nintendo.co.uk{url}",
+                        "Price": price,
+                        "Stock": "",
+                        "Website": self.name,
+                    }
 
-                        found = {
-                            "Colour": 0x0000FF,
-                            "Title": name,
-                            "Image": f"https://assets.nintendo.eu/image/upload/v1654696477/{img}",
-                            "URL": f"https://store.nintendo.co.uk{url}",
-                            "Price": price,
-                            "Stock": "",
-                            "Website": self.name,
-                        }
+                    if stock == "OutOfStock":
+                        found["Colour"] = 0xFF0000
+                        found["Stock"] = Stock.OUT_OF_STOCK.value
+                    else:
+                        found["Colour"] = 0x00FF00
+                        found["Stock"] = Stock.IN_STOCK.value
 
-                        if stock == "OutOfStock":
-                            found["Colour"] = 0xFF0000
-                            found["Stock"] = Stock.OUT_OF_STOCK.value
-                        else:
-                            found["Colour"] = 0x00FF00
-                            found["Stock"] = Stock.IN_STOCK.value
+                    if found not in all_found:
+                        all_found.append(found)
+                self.params["offset"] += 24
+            else:
+                log.info("Requests library failed, attempting with selenium")
+                self.base_url = (
+                    "https://store.nintendo.co.uk/en_gb/games/enhance-your-play/amiibo/"
+                )
 
-                        if found not in all_found:
-                            all_found.append(found)
-                else:
-                    log.info("Requests library failed, attempting with selenium")
-                    self.base_url = "https://store.nintendo.co.uk/en_gb/games/enhance-your-play/amiibo/"
+                response = self.scrape_with_selenium(
+                    url=self.base_url, payload=self.params
+                )
+                soup = BeautifulSoup(response, "html.parser")
+                cards = soup.find_all(
+                    "div",
+                    attrs={"class": lambda e: e.startswith("col-6") if e else False},
+                )
 
-                    response = self.scrape_with_selenium(
-                        url=self.base_url, payload=self.params
-                    )
-                    soup = BeautifulSoup(response, "html.parser")
-                    cards = soup.find_all(
-                        "div",
+                for card in cards:
+                    name = card.find_all(
+                        "h3",
                         attrs={
-                            "class": lambda e: e.startswith("col-6") if e else False
+                            "class": lambda e: e.startswith("ProductCard_card__title")
+                            if e
+                            else False
+                        },
+                    )
+                    stock = card.find_all(
+                        "button",
+                        attrs={
+                            "class": lambda e: e.startswith("ProductCard_btn")
+                            if e
+                            else False
+                        },
+                    )
+                    price = card.find_all(
+                        "data",
+                        attrs={
+                            "class": lambda e: e.startswith("value") if e else False
+                        },
+                    )
+                    img = card.find_all("img")
+                    url = card.find_all(
+                        "a",
+                        attrs={
+                            "class": lambda e: e.startswith("ProductCard_card__link")
+                            if e
+                            else False
                         },
                     )
 
-                    for card in cards:
-                        name = card.find_all(
-                            "h3",
-                            attrs={
-                                "class": lambda e: e.startswith(
-                                    "ProductCard_card__title"
-                                )
-                                if e
-                                else False
-                            },
-                        )
-                        stock = card.find_all(
-                            "button",
-                            attrs={
-                                "class": lambda e: e.startswith("ProductCard_btn")
-                                if e
-                                else False
-                            },
-                        )
-                        price = card.find_all(
-                            "data",
-                            attrs={
-                                "class": lambda e: e.startswith("value") if e else False
-                            },
-                        )
-                        img = card.find_all("img")
-                        url = card.find_all(
-                            "a",
-                            attrs={
-                                "class": lambda e: e.startswith(
-                                    "ProductCard_card__link"
-                                )
-                                if e
-                                else False
-                            },
-                        )
+                    if name and stock and price and img and url:
+                        name = name[0]
+                        stock = stock[0]
+                        price = price[0]
+                        img = img[0]
+                        url = url[0]
+                    else:
+                        continue
 
-                        if name and stock and price and img and url:
-                            name = name[0]
-                            stock = stock[0]
-                            price = price[0]
-                            img = img[0]
-                            url = url[0]
-                        else:
-                            continue
+                    found = {
+                        "Colour": 0x0000FF,
+                        "Title": name.text.strip(),
+                        "Image": img["src"].strip(),
+                        "URL": f"https://store.nintendo.co.uk/{url['href'].strip()}",
+                        "Price": price.text.strip(),
+                        "Stock": "",
+                        "Website": self.name,
+                    }
 
-                        found = {
-                            "Colour": 0x0000FF,
-                            "Title": name.text.strip(),
-                            "Image": img["src"].strip(),
-                            "URL": f"https://store.nintendo.co.uk/{url['href'].strip()}",
-                            "Price": price.text.strip(),
-                            "Stock": "",
-                            "Website": self.name,
-                        }
+                    if 'disabled=""' in str(stock):
+                        found["Colour"] = 0xFF0000
+                        found["Stock"] = Stock.OUT_OF_STOCK.value
+                    else:
+                        found["Colour"] = 0x00FF00
+                        found["Stock"] = Stock.IN_STOCK.value
 
-                        if 'disabled=""' in str(stock):
-                            found["Colour"] = 0xFF0000
-                            found["Stock"] = Stock.OUT_OF_STOCK.value
-                        else:
-                            found["Colour"] = 0x00FF00
-                            found["Stock"] = Stock.IN_STOCK.value
-
-                        if found not in all_found:
-                            all_found.append(found)
+                    if found not in all_found:
+                        all_found.append(found)
 
         return all_found
