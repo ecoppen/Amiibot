5f60f84ece8a516f1c9341e6047d437d295f75e2
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-12-04 10:52:35 +0000

    Update nintendouk.py
    Added check for `None` and logged. Remove selenium scrape for now

--- stockist/nintendouk.py
+++ stockist/nintendouk.py
@@ -45,7 +45,12 @@ class NintendoUK(Stockist):
 
             if len(cards) > 0:
                 if "data" in [*cards]:
+                    log.info(f'{cards["data"]}')
+                    if cards["data"] is None:
+                        log.warning("No data returned from API")
+                        break
                     if "products" in cards["data"]:
+                        log.info(f'{cards["data"]["products"]}')
                         if len(cards["data"]["products"]) == 0:
                             complete = True
                         for card in cards["data"]["products"]:
@@ -79,76 +84,4 @@ class NintendoUK(Stockist):
                             break
             else:
                 break
-        if len(all_found) == 0:
-            log.info("Requests library failed, attempting with selenium")
-            self.base_url = (
-                "https://store.nintendo.co.uk/en_gb/games/enhance-your-play/amiibo/"
-            )
-
-            response = self.scrape_with_selenium(url=self.base_url, payload=self.params)
-            soup = BeautifulSoup(response, "html.parser")
-            cards = soup.find_all(
-                "div",
-                attrs={"class": lambda e: e.startswith("col-6") if e else False},
-            )
-
-            for card in cards:
-                name = card.find_all(
-                    "h3",
-                    attrs={
-                        "class": lambda e: e.startswith("ProductCard_card__title")
-                        if e
-                        else False
-                    },
-                )
-                stock = card.find_all(
-                    "button",
-                    attrs={
-                        "class": lambda e: e.startswith("ProductCard_btn")
-                        if e
-                        else False
-                    },
-                )
-                price = card.find_all(
-                    "data",
-                    attrs={"class": lambda e: e.startswith("value") if e else False},
-                )
-                img = card.find_all("img")
-                url = card.find_all(
-                    "a",
-                    attrs={
-                        "class": lambda e: e.startswith("ProductCard_card__link")
-                        if e
-                        else False
-                    },
-                )
-
-                if name and stock and price and img and url:
-                    name = name[0]
-                    stock = stock[0]
-                    price = price[0]
-                    img = img[0]
-                    url = url[0]
-                else:
-                    continue
-
-                found = {
-                    "Colour": 0x0000FF,
-                    "Title": name.text.strip(),
-                    "Image": img["src"].strip(),
-                    "URL": f"https://store.nintendo.co.uk/{url['href'].strip()}",
-                    "Price": price.text.strip(),
-                    "Stock": "",
-                    "Website": self.name,
-                }
-
-                if 'disabled=""' in str(stock):
-                    found["Colour"] = 0xFF0000
-                    found["Stock"] = Stock.OUT_OF_STOCK.value
-                else:
-                    found["Colour"] = 0x00FF00
-                    found["Stock"] = Stock.IN_STOCK.value
-
-                if found not in all_found:
-                    all_found.append(found)
         return all_found
